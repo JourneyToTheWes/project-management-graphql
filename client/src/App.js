ad59d98d0e5265d29f34f985d5c107e69b03292d
Wesley Wei <wesley631w@gmail.com>
2024-03-11 12:39:08 -0700

    Implemented Delete client mutation and fixed InMemoryCache issue - Added Spinner for loading data - Refactored Queries and Mutations to their own directories - Added delete client to the ClientRow UI and added logic to dynamically   update the UI once a delete occurs

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,44 @@ import Header from './components/Header';
 import Clients from './components/Clients';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+// Fixes Cache data lost error message when replacing clients field of
+// a Query object. Prevents console problems when network requests are
+// made to fetch data that were cached.
+const cache = new InMemoryCache({
+    typePolicies: {
+        Query: {
+            fields: {
+                clients: {
+                    merge(existing, incoming) {
+                        return incoming;
+                    }
+                },
+                projects: {
+                    merge(existing, incoming) {
+                        return incoming;
+                    }
+                }
+            }
+        }
+    }
+});
+
 const client = new ApolloClient({
-  uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache()
+    uri: 'http://localhost:5000/graphql',
+    cache
 });
 
 const App = () => {
-  return (
-    <>
-      <ApolloProvider client={client}>
-        <Header />
-        <div className="container">
-          <Clients />
-        </div>
-      </ApolloProvider>
-    </>
-  );
+    return (
+        <>
+            <ApolloProvider client={client}>
+                <Header />
+                <div className="container">
+                <Clients />
+                </div>
+            </ApolloProvider>
+        </>
+    );
 };
 
 export default App;
